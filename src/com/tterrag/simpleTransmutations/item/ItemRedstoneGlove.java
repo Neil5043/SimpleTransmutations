6592ef05d058a943fdcb700c0682b67450ca1033
tterrag1098 <tterrag1098@gmail.com>
2013-09-25 21:54:40 -0400

    added invisible redstone logic

--- src/com/tterrag/simpleTransmutations/item/ItemRedstoneGlove.java
+++ src/com/tterrag/simpleTransmutations/item/ItemRedstoneGlove.java
@@ -4,8 +4,10 @@ import com.tterrag.simpleTransmutations.block.BlockInfo;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -16,12 +18,23 @@ import net.minecraft.world.World;
 @SuppressWarnings("unused")
 public class ItemRedstoneGlove extends Item{
 	
+	private float weaponDamage;
+	
 	public ItemRedstoneGlove (int id)
 	{
 		super(id);
 		setCreativeTab(CreativeTabs.tabTools);
 		setMaxStackSize(1);
 		setUnlocalizedName(ItemInfo.REDSTONE_GLOVE_UNLOC_NAME);
+		setMaxDamage(100);
+		this.weaponDamage = 4.0F;
+	}
+	
+	@Override
+	public float getDamageVsEntity(Entity par1Entity, ItemStack itemStack)
+	{
+		System.out.println("called");
+		return this.weaponDamage;	
 	}
 	
 	@Override
@@ -32,13 +45,124 @@ public class ItemRedstoneGlove extends Item{
 	}
 	
 	@Override
-	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) 
+	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player,
+			World world, int x, int y, int z, int side, float hitX, float hitY,
+			float hitZ)
 	{
-		if (!world.isRemote && world.isAirBlock(x, y + 1, z))
+		stack.damageItem(1, player);
+		if (!world.isRemote && (world.getBlockId(x, y, z) != Block.pistonBase.blockID && world.getBlockId(x, y, z) != Block.pistonStickyBase.blockID || world.getBlockMetadata(x, y, z) != 1))
 			{
-				world.setBlock(x, y + 1, z, BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
-				return false;
+			for (int i = 0; i < 6; i++)
+				{
+				switch (i)
+				{
+				case 0:
+					if (world.isAirBlock(x, y + 1, z))
+					{
+						world.setBlock(x, y + 1, z,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				case 1:
+					if (world.isAirBlock(x + 1, y, z))
+					{
+						world.setBlock(x + 1, y, z,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				case 2:
+					if (world.isAirBlock(x, y, z + 1))
+					{
+						world.setBlock(x, y, z + 1,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				case 3:
+					if (world.isAirBlock(x, y, z - 1))
+					{
+						world.setBlock(x, y, z - 1,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				case 4:
+					if (world.isAirBlock(x - 1, y, z))
+					{
+						world.setBlock(x - 1, y, z,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				case 5:
+					if (world.isAirBlock(x, y - 1, z))
+					{
+						world.setBlock(x, y - 1, z,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				default:
+					return false;
+				}
 			}
-		return false;
+		}
+		else if (!world.isRemote && (world.getBlockId(x, y, z) == Block.pistonBase.blockID || world.getBlockId(x, y, z) == Block.pistonStickyBase.blockID) && world.getBlockMetadata(x, y, z) == 1)
+		{
+			for (int i = 0; i < 6; i++)
+			{
+				switch (i)
+				{
+				case 0:
+					break;
+				case 1:
+					if (world.isAirBlock(x + 1, y, z))
+					{
+						world.setBlock(x + 1, y, z,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				case 2:
+					if (world.isAirBlock(x, y, z + 1))
+					{
+						world.setBlock(x, y, z + 1,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				case 3:
+					if (world.isAirBlock(x, y, z - 1))
+					{
+						world.setBlock(x, y, z - 1,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				case 4:
+					if (world.isAirBlock(x - 1, y, z))
+					{
+						world.setBlock(x - 1, y, z,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				case 5:
+					if (world.isAirBlock(x, y - 1, z))
+					{
+						world.setBlock(x, y - 1, z,
+								BlockInfo.INVISIBLE_REDSTONE_ID, 0, 3);
+						return false;
+					}
+					break;
+				default:
+					return false;
+				}
+			}
+		}
+		else return false;
+		return true;
 	}
 }
